d305888f86e5f6ec2eded45d081d822c82126a21
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-11-21 22:14:48 -0800

    init

---
+++ launch-beammp-linux-example.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# These two must be on the same drive
+protonModsFolder="$HOME/.steam/steam/steamapps/compatdata/284160/pfx/drive_c/users/$USER/AppData/Local/BeamNG.drive/0.26/mods"
+nativeModsFolder="$HOME/.local/share/BeamNG.drive/0.26/mods"
+
+# Lutris game identifiers
+lutrisbeammpid="beammp"
+lutrisbeamnglinuxid="beamng-linux"
+
+# Launch BeamNG Linux and BeamMP from lutris.
+echo "Launching BeamNG Native Linux"
+lutris lutris:rungame/$lutrisbeamnglinuxid
+# Sleep for 8s to prevent race condition. On my computer, this usually makes it so that BeamMP starts right on time.
+sleep 8
+echo "Launching BeamMP"
+lutris lutris:rungame/$lutrisbeammpid
+
+echo "Removing last time's mods"
+cd $nativeModsFolder/mods
+find . ! -name beammp.zip ! -name BeamMP.zip -maxdepth 1 -type f -delete
+
+echo "Watching for mods that need to be loaded"
+inotifywait -m -e create -e moved_to --format "%f" $protonModsFolder/multiplayer \
+        | while read filename
+                do
+                        echo "Detected mod $filename, hard linking in 0.5s"
+			sleep 0.5
+                        ln $protonModsFolder/multiplayer/$filename $nativeModsFolder/multiplayer/$filename
+                done
---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+function yes_or_no {
+    while true; do
+        read yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) return 1  ;;
+        esac
+    done
+}
+
+echo "Checking for dependencies ..."
+if [[ $(which lutris) != "" && $(which inotifywait) != "" ]]; then
+    echo "Dependencies found."
+else    
+    echo "Dependencies not found. Please install lutris and inotify-tools"
+fi
+
+echo "Hello, please follow https://www.reddit.com/r/BeamMP/comments/oi67rf/beammp_on_linux_guide/ to setup BeamMP. Press enter when this is done."
+read
+
+echo "Is $HOME/.steam/steam/steamapps/compatdata/284160/pfx/drive_c/users/$USER/AppData/Local/BeamNG.drive/0.26/mods/ is this the path of BeamNG.Drive's mods folder for proton? [y/n]"
+
+if ! yes_or_no; then
+    read -p "Please enter BeamNG.Drive's mods folder for proton: " protonpath
+else
+    protonpath="$HOME/.steam/steam/steamapps/compatdata/284160/pfx/drive_c/users/$USER/AppData/Local/BeamNG.drive/0.26/mods"
+fi
+
+echo "Is $HOME/.local/share/BeamNG.drive/0.26/mods/ is this the path of BeamNG.Drive's mods folder for native? [y/n]"
+
+if ! yes_or_no; then
+    read -p "Please enter BeamNG.Drive's mods folder for proton: " nativepath
+else
+    nativepath="$HOME/.local/share/BeamNG.drive/0.26/mods"
+fi
+
+read -p "Please enter the lutris identifier for BeamMP: " beammpid 
+
+read -p "Please enter the lutris identifier for BeamNG.drive native Linux build: " beamngid 
+
+mkdir -p $nativepath/multiplayer
+
+echo "Downloading latest version of BeamMP. You will need to manually update this by downloading beammp.zip from https://github.com/BeamMP/BeamMP/releases/latest and putting it in $nativepath/multiplayer, replacing the old one."
+wget https://github.com/BeamMP/BeamMP/releases/latest/download/beammp.zip -O $nativepath/multiplayer/beammp.zip
+
+echo "Generating a BeamMP launch script for you ..."
+cat launch-beammp-linux-template.sh | sed "s:PMODS:$protonpath:g" | sed "s:NMODS:$nativepath:g" | sed "s:MPID:$beammpid:g" | sed "s:MPID:$beammgid:g" > beammp-linux.sh
+chmod +x beammp-linux.sh
+echo "Done. Run beammp-linux.sh with ./beammp-linux.sh to start BeamMP."
